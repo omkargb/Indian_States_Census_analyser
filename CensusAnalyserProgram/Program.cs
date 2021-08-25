20eed9483975a3f6ace4f19aeceffcd3adde198c
omkargb <omkargb107@gmail.com>
2021-08-25 16:32:29 +0530

    [omkargb] Add : Program files added and welcome message displayed.

---
+++ CensusAnalyserProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CensusAnalyserProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Indian States Census analyser program \n");
+        }
+    }
+}
